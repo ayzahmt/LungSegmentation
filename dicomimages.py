3b33662f38a408ade6014378e456eeedbd2aa7f7
ayzahmt <ayazahmet34@gmail.com>
2020-10-06 23:32:32 +0300

    added get_pixels_hu method

--- dicomimages.py
+++ dicomimages.py
@@ -20,8 +20,31 @@ def load_images(path):
     return slices
 
 
+#
+# Convert to HU values
+#
+def get_pixels_hu(scans):
+    image = np.stack([s.pixel_array for s in scans])
+    image = image.astype(np.int16)
 
+    intercept = scans[0].RescaleIntercept
+    slope = scans[0].RescaleSlope
+
+    if slope != 1:
+        image = slope * image.astype(np.float64)
+        image = image.astype(np.int16)
+
+    image += np.int16(intercept)
+
+    return np.array(image, dtype=np.int16)
+
+
+
+#######
+# Initialize parameter and run methods
+#######
 p1_dicom_path = "C:\\Users\\Ayaz\\Desktop\\msc\\scans\\scans\\Adem Acar\\12-12-2016 bt\\DICOM\\ST000000\\SE000003\\"
 
 
-load_images(p1_dicom_path)
+patient = load_images(p1_dicom_path)
+imgs = get_pixels_hu(patient)
