48a23fe3ef7ec30c87d2830d9eb79a4eb11512ef
Ahmet <ayazahmet34@gmail.com>
2020-11-21 21:48:49 +0200

    added segment_lung method

--- run.py
+++ run.py
@@ -5,13 +5,16 @@ import segmentation
 #######
 # Initialize parameter and run methods
 #######
-p1_dicom_path = "C:\\Users\\Ayaz\\Desktop\\msc\\scans\\scans\\Adem Acar\\12-12-2016 bt\\DICOM\\ST000000\\SE000003\\"
-data_path = "C:\\Users\\Ayaz\\Desktop\\tez\\dataset\\"
+p1_dicom_path = "C:\\Users\\Ahmet\\Desktop\\tez_verileri\\msc\\scans\\scans\\Adem Acar\\12-12-2016 bt\\DICOM\\ST000000\\SE000003\\"
+data_path = "C:\\Users\\Ahmet\\Desktop\\tez_verileri\\tez\\dataset\\"
 
 # patient = dicomimages.load_images(p1_dicom_path)
 # imgs = dicomimages.get_pixels_hu(patient)
 # dicomimages.save_images_array(data_path, imgs)
 
-imgs = dicomimages.load_images_array(data_path, 2020109)
-imgs = segmentation.create_mask(imgs)
+# imgs = dicomimages.load_images_array(data_path, 2020109)
+# imgs = segmentation.create_mask(imgs)
+
+imgs = segmentation.segment_lung(p1_dicom_path)
+
 visualization.show_one_slice(imgs[30])
--- segmentation.py
+++ segmentation.py
@@ -1,32 +1,32 @@
 import numpy as np
 from skimage import measure
+import dicomimages
+
 
 def create_mask(image):
-
-    #piksel değerlerini 1 ve 2 yapıyoruz
-    #backround olarak default 0 değeri set edilir
+    # piksel değerlerini 1 ve 2 yapıyoruz
+    # backround olarak default 0 değeri set edilir
     binary_image = np.array(image > -300, dtype=np.int8) + 1
 
-    #label backround değerini default 0 alıyor
-    #piksel değerlerini etiketliyor (1 den başlıyor)
+    # label backround değerini default 0 alıyor
+    # piksel değerlerini etiketliyor (1 den başlıyor)
     labels = measure.label(binary_image)
 
-    #backround için en köşeden seçilir
+    # backround için en köşeden seçilir
     background_label = labels[0, 0, 0]
 
     binary_image[background_label == labels] = 2
 
-
     # Measure.label fonksiyonu ile en büyük doku belirlenir ve geri kalanı doldurulur (0 ya da 1)
     for i, single_slice in enumerate(binary_image):
-        #pikselleri 1 ve 2 yapmıştık (+1 eklemiştik). Şimdi eski haline çeviriyoruz.
+        # pikselleri 1 ve 2 yapmıştık (+1 eklemiştik). Şimdi eski haline çeviriyoruz.
         single_slice = single_slice - 1
         labeling = measure.label(single_slice)
         # backround = 0. Eğer 0 dan büyük değer varsa etiket değerini dön
         l_max = largest_label_volume(labeling, bg=0)
 
         if l_max is not None:  # akciğer hücresi var demek. akciger = 2
-            binary_image[i][labeling != l_max] = 1 # akciğer etiketli olmayanları 1 yap
+            binary_image[i][labeling != l_max] = 1  # akciğer etiketli olmayanları 1 yap
 
     # pikselleri 1 ve 2 yapmıştık (+1 eklemiştik). Şimdi eski haline çeviriyoruz.
     binary_image -= 1
@@ -37,18 +37,31 @@ def create_mask(image):
     # backround = 0. Eğer 0 dan büyük değer varsa etiket değerini dön
     l_max = largest_label_volume(labels, bg=0)
     if l_max is not None:
-        binary_image[labels != l_max] = 0 # akciğer etiketli olmayanları 0 yap
+        binary_image[labels != l_max] = 0  # akciğer etiketli olmayanları 0 yap
 
     return binary_image
 
 
 def largest_label_volume(im, bg=-1):
-    vals, counts = np.unique(im, return_counts=True)
+    values, counts = np.unique(im, return_counts=True)
 
-    counts = counts[vals != bg]
-    vals = vals[vals != bg]
+    counts = counts[values != bg]
+    values = values[values != bg]
 
     if len(counts) > 0:
-        return vals[np.argmax(counts)]
+        return values[np.argmax(counts)]
     else:
         return None
+
+
+def segment_lung(dicom_path):
+    imgs = dicomimages.load_images(dicom_path)
+    imgs_with_hu = dicomimages.get_pixels_hu(imgs)
+
+    masks = create_mask(imgs_with_hu)
+
+    imgs_segmented_lung = imgs_with_hu * masks
+
+    return imgs_segmented_lung
+
+
