967878e6a440cade861200e64eb67d408f2a3d0d
Ahmet <ayazahmet34@gmail.com>
2020-12-06 23:44:15 +0300

    tested dice_metric method

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ p4_nifti_path = "C:\\Users\\Ahmet\\Desktop\\tez_verileri\\msc\\isaretlemeler\\3a
 p4_dicom_path = "C:\\Users\\Ahmet\\Desktop\\tez_verileri\\msc\\\scans\\scans\\Fatma Demirsoy"
 
 
-paths = [PatientPath(p1_dicom_path, p1_nifti_path)
+paths = [PatientPath(p2_dicom_path, p2_nifti_path)
          #,PatientPath(p2_dicom_path, p2_nifti_path)
          #,PatientPath(p3_dicom_path, p3_nifti_path)
          #,PatientPath(p4_dicom_path, p4_nifti_path)
@@ -38,14 +38,21 @@ paths = [PatientPath(p1_dicom_path, p1_nifti_path)
 
 dices = []
 def dice_metric(segmented_images, labeled_images):
-    labeled_images_transpose = np.transpose(labeled_images, (2, 1, 0))
 
-    for i in range(0, len(segmented_lungs)):
-        dice = segmentation.dice_metric_coeffecient(segmented_images[i], labeled_images_transpose[i])
+    middle_slice_index = int(len(segmented_images) // 2)
+    begin = middle_slice_index_per_5_begin = middle_slice_index - int(middle_slice_index * 10 / 100)
+    end = middle_slice_index_per_5_end = middle_slice_index + int(middle_slice_index * 10 / 100)
+
+    slice_count = 0
+
+    for i in range(begin, end+1):
+        dice = segmentation.dice_metric_coeffecient(segmented_images[i], labeled_images[i])
         dices.append(dice)
-        print(dice)
+        print(str(i) + ".slice - " + str(dice))
+        slice_count = slice_count + 1
 
-    return dices
+    print(str(slice_count) + " tane slice için Dice metrix ortalama:", sum(dices) / len(dices))
+    print()
 
 
 for path in paths:
@@ -59,13 +66,17 @@ for path in paths:
 
     segmented_lungs = segmentation.segment_lung(images_with_hu)
 
+    #segmented_lungs = images_with_hu * segmented_lungs
+
     #visualization.show_masked_lung(images)
 
     #visualization.show_nifti_lung(labeled_images)
 
-    visualization.plot_dicom_and_nifti(images, labeled_images, segmented_lungs)
+    #visualization.plot_dicom_and_nifti(images, labeled_images, segmented_lungs)
 
-    #dice = dice_metric(segmented_lungs, labeled_images)
+    #visualization.plot_nifti(labeled_images)
+
+    dice_metric(segmented_lungs, labeled_images)
 
     #print("Dice metrix average for patient:", sum(dice) / len(dice))
 
